6571454cbd716493a7d215df2192e8c488f7579d
quangsang <whitefang.tcotw@gmail.com>
2015-12-16 19:48:03 +0700

    Dynamic Animation Rate

--- NewSuperMarioBrosPC/animation.h
+++ NewSuperMarioBrosPC/animation.h
@@ -1,17 +1,23 @@
-#ifndef _ANIMATION_H
+﻿#ifndef _ANIMATION_H
 #define _ANIMATION_H
 
 class Animation
 {
+private:
+	int frameDelayCounter;
 public:
-	static const int FRAME_DELAY;
+	static const int FRAME_DELAY_DEFAULT = 6;//default =6
 	int startFrame, endFrame;
 	int index;
-	int frameDelay;
+	int frameDelay;		//để kiểm soát tốc độ chuyển frame, 
+						//ở một số trạng thái thì object có animation nhanh chậm khác nhau
+						//ví dụ khi mario chạy thì animation nhanh hơn bình thường
+						//hay Rùa ở khi Slide Anim nhanh hơn Rùa đi bộ
 
-	Animation(int startFrame, int endFrame);
+	Animation(int startFrame, int endFrame, int frameDelay = Animation::FRAME_DELAY_DEFAULT);
 	~Animation();
 	void Update();
 	void SetIndex(int index);
+	void SetFrameDeplay(int frameDelay);
 };
 #endif
